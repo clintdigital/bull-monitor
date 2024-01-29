1f7e23aedf0dcd9940bc41e3ebd33948ba0f41d7
Kadio Francisco <kadiofrancisco@Kadio-MacBook.local>
2024-01-29 17:27:03 -0300

    add more queues

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,6 @@ bull-monitor \
   --prefix bull \
   --bullmq \
   -q ImportCsv -q ImportDeals -q ImportContacts \
-  -q MergeContacts -q MergeContactFirstStep -q MergeContactFinalStep
+  -q MergeContacts -q MergeContactFirstStep -q MergeContactFinalStep \
+  -q ImportTrackDeal  -q ImportTrackContact \
+  -q CampaignProcessContacts -q CampaignFinish -q CampaignSendContactToProvider -q CampaignValidateFinish
